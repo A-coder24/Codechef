b037474a0615ec7ebc41d5c8ba9efd10231d4713
unknown <sanagrawal10401@gmail.com>
2020-06-19 21:06:31 +0530

    June long challenge

--- second.py
+++
@@ -1,7 +0,0 @@
-def f():
-    return map(int ,input().split())
-def lis():
-    return list(map(int ,input().split()))
-""".........................................."""
-for i in range(200000):
-    print(3,end=" ")
